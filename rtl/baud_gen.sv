68429a83a5f9f949a80b60df6275f0aedf8a6485
Amartya Gangwar <amartyagangwar1@gmail.com>
2026-02-25 15:23:21 -0600

    Parametrized Baud Rate Tick Generator and converted test bench to be self checking

--- rtl/baud_gen.sv
+++ rtl/baud_gen.sv
@@ -1,27 +1,29 @@
 `timescale 1ns / 1ps
 
-//count to 868 and then pulse tick_baud
+module baud_gen #(parameter int CLK_FREQ = 100000000, parameter int BAUD_RATE = 115200)
+                 (input logic clk, input logic rst, output logic tick_baud);
 
-module baud_gen(
-    input logic clk,                                    //100 MHZ Clock from FPGA
-    input logic rst,                                    //Reset Button 
-    output logic tick_baud                              //One pulse every 100 MHZ / 115200 HZ or Clock Rate / Baud Rate = 868
-    );
-    
-    logic [10:0] count = 0;                             //Need at least 2^10 bits or 1024 bits to count up to 868
-    int cmp = 868;                                      //Value we are counting up to
-    
-    always_ff @(posedge clk) begin                      //Every clock tick we are incrementing the counter
-        if(rst) begin                                   //If reset button active, change count and tick baud to 0 
-            count <= 0;
-            tick_baud <=0;
-        end else if (count == (cmp-1)) begin            //If count is at 868 reset back to 0 and pulse tick_baud
-            count <=0;
-            tick_baud <=1;
-        end else begin                                  //Regular count up
+    localparam int DIV = CLK_FREQ / BAUD_RATE; //Number of clock cycles per baud tick
+
+    initial begin if (DIV < 1)
+            $fatal("Invalid parameter combination");
+    end
+
+    localparam int CNT_WIDTH = (DIV > 1) ? $clog2(DIV): 1; //Number of minimum bits required to store the values from 0 till DIV-1
+
+    logic [(CNT_WIDTH-1):0] count;
+
+    always_ff @(posedge clk) begin
+        if(rst) begin //Synchronous Reset
+            count <= '0;
+            tick_baud <= '0;
+        end else if (count == (DIV-1)) begin
+            count <= '0;
+            tick_baud <= '1;
+        end else begin
             count <= count + 1;
-            tick_baud <= 0;
-        end     
-    end    
-    
+            tick_baud <= '0;
+        end
+    end
+
 endmodule
--- tb/baud_gen_TB.sv
+++ tb/baud_gen_TB.sv
@@ -2,25 +2,58 @@
 
 module baud_gen_TB();
 
-    //tb signals
+    parameter int CLK_FREQ = 100000000;
+    parameter int BAUD_RATE = 115200;
+    localparam int DIV = CLK_FREQ / BAUD_RATE;
+
     logic clk;
     logic rst;
     logic tick_baud;
-    
+
     //instantiate dut
-    baud_gen dut(.clk(clk), .rst(rst), .tick_baud(tick_baud));
-    
+    baud_gen #(.CLK_FREQ(CLK_FREQ), .BAUD_RATE(BAUD_RATE)) dut (.clk(clk), .rst(rst), .tick_baud(tick_baud));
+
     //generate clock to be 100 mhz or 10 ns period
     initial clk = 0;
     always #5 clk = ~clk;
-    
+
     //stimulus, reset system and then allow for enough run time to see clock pulses
     initial begin
         rst = 1;
-        #20;
+        repeat (5) @(posedge clk);
         rst = 0;
-        
-        #2000;
-        $stop;
+
+        #0.2ms
+        //repeat (5000000000) @(posedge clk);
+        $display("PASSED");
+        $finish;
+    end
+
+
+    int cycle_count;
+    logic first_tick_seen;
+
+    always @(posedge clk) begin
+        if(rst) begin
+            cycle_count <= 0;
+            first_tick_seen <=0;
+        end else begin
+            if(tick_baud) begin
+                if(first_tick_seen) begin
+                    //$display("TICK cycles %d, DIV %d", cycle_count, DIV);
+                    if(cycle_count + 1 == DIV) begin
+                        cycle_count <=0;
+                        //$display("Cycle count correct");
+                    end else begin
+                        cycle_count <= 0;
+                        $fatal(1, "ERROR, Cycles: %d, DIV: %d", cycle_count, DIV);
+                    end
+                end else begin
+                    first_tick_seen <=1;
+                    cycle_count <=0;
+                end
+            end else
+                cycle_count <= cycle_count + 1;
+        end
     end
 endmodule
